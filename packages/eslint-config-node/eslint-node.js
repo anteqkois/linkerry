2b96325b42a147825c7e7d8323802f134e617a17
anteqkois <anteqkois.dev@gmail.com>
2023-06-19 18:05:05 +0200

    build(repo): init repo, add basic dependencies and config

---
+++ packages/eslint-config-node/eslint-node.js
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:security/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.js', '.ts'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        project: '.',
+      },
+    },
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'import/group-exports': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/no-unused-expressions': ['error', { allowTernary: true, allowShortCircuit: true }],
+    'no-console': ['error', { allow: ['log', 'warn', 'error'] }],
+    'no-restricted-syntax': 'off',
+    'no-underscore-dangle': ['error', { allow: ['_count'] }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-throw-literal': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-empty-interface': [
+      'error',
+      {
+        allowSingleExtends: true,
+      },
+    ],
+    'class-methods-use-this': 'off',
+  },
+};
