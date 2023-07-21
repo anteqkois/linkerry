d03858b60c7dad91c9874827274bbba7b85d369d
anteqkois <anteqkois.dev@gmail.com>
2023-07-21 22:21:33 +0200

    feat(web): setup nextjs app, add tailwind css

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,14 @@
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div>
+      <h1 className="text-purple-600 text-7xl">
+      Hello from Niecew
+      </h1>
+    </div>
+  )
+}
