0f91c98d020ae82db83164f891b78f383b49e223
anteqkois <anteqkois.dev@gmail.com>
2023-07-30 14:47:13 +0200

    refactor(architecture): refactor strategy-buy model, add exchange keys model

--- libs/types/src/models/strategy-buy.ts
+++ libs/types/src/models/strategy-buy.ts
@@ -1,45 +1,58 @@
-import { type } from 'os'
 import { ICondition } from './condition'
 import { IMarket } from './market'
 import { IStrategySell } from './strategy-sell'
 
-export enum StrategyBuyTypeType {
-  oneTicker = 'oneTicker',
-  manyTicker = 'manyTicker',
-}
+// export enum StrategyBuyTypeType {
+//   ONE_SYMBOL = 'oneSymbol',
+//   MANY_SYMBOLS = 'manySymbols',
+// }
 
 export enum StrategyBuyMarketStrategyType {
-  dynamic = 'dynamic',
-  static = 'static',
+  DYNAMIC = 'dynamic',
+  STATIC = 'static',
 }
 
-export type StrategyWithMarket = {
-  marketStrategy: StrategyBuyMarketStrategyType.static
-  market: string
-  marketProperty: {
+interface IStrategyWithMarket {
+  market?: string // Id to market in db OR in fiture array to ids
+  marketProperty?: {
     id: IMarket['id']
     exchangeId: IMarket['exchangeId']
     symbol: IMarket['symbol']
   }
 }
 
-type StrategyWithMarketPupulated = {
-  marketStrategy: StrategyBuyMarketStrategyType.static
-  market: string
-  marketProperty: {
-    id: IMarket['id']
-    exchangeId: IMarket['exchangeId']
-    symbol: IMarket['symbol']
-  }
+interface IStrategyWithDynamicMarket {
+  conditionMarketProvider?: string
 }
 
-type StrategyWithDynamicMarket = {
-  marketStrategy: StrategyBuyMarketStrategyType.dynamic
+// # # # # # # # # # #
+
+export interface IStrategyBuy extends IStrategyWithMarket, IStrategyWithDynamicMarket {
+  // type: StrategyBuyTypeType
+  strategySell: IStrategySell
+  marketStrategy: StrategyBuyMarketStrategyType
+}
+
+// # # # # # # # # # #
+
+export interface IStrategyBuyStaticMarket
+  extends Omit<IStrategyBuy, keyof IStrategyWithMarket>,
+    Required<IStrategyWithMarket> {
+  marketStrategy: StrategyBuyMarketStrategyType.STATIC
+}
+
+export interface IStrategyBuyStaticMarketPopulated extends Omit<IStrategyBuyStaticMarket, 'market'> {
+  market: IMarket
+}
+
+// # # # # # # # # # #
+
+export interface IStrategyBuyDynamicMarket
+  extends Omit<IStrategyBuy, keyof IStrategyWithDynamicMarket>,
+    Required<IStrategyWithDynamicMarket> {
+  marketStrategy: StrategyBuyMarketStrategyType.DYNAMIC
+}
+
+export interface IStrategyBuyDynamicMarketPopulated extends Omit<IStrategyBuyDynamicMarket, 'conditionMarketProvider'> {
   conditionMarketProvider: ICondition
 }
-
-// Stay with convenction I... :/
-export type IStrategyBuy = {
-  type: StrategyBuyTypeType
-  strategySell: IStrategySell
-} & (StrategyWithMarket | StrategyWithDynamicMarket)
