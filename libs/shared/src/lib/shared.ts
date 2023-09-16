40df5a87d50c6d3aa813a82bfa1c4ab762ed2938
anteqkois <anteqkois.dev@gmail.com>
2023-09-16 17:42:26 +0200

    feat(apps): add flow-executor and api-gateway

--- libs/shared/src/lib/shared.ts
+++ libs/shared/src/lib/shared.ts
@@ -3,25 +3,29 @@ export type Id = string
 export type ProjectId = Id
 
 export function isNil<T>(value: T | null | undefined): value is null | undefined {
-  return value === null || value === undefined;
+  return value === null || value === undefined
 }
 
 export function isString(str: unknown): str is string {
-  return str != null && typeof str === "string";
+  return str != null && typeof str === 'string'
 }
 
 export function isEmpty<T>(value: T | null | undefined): boolean {
   if (value == null) {
-    return true;
+    return true
   }
 
   if (typeof value === 'string' || Array.isArray(value)) {
-    return value.length === 0;
+    return value.length === 0
   }
 
   if (typeof value === 'object') {
-    return Object.keys(value).length === 0;
+    return Object.keys(value).length === 0
   }
 
-  return false;
+  return false
 }
+
+export type Prettify<T> = {
+  [K in keyof T]: T[K]
+} & {}
