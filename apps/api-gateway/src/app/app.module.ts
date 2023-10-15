e06bfcebb3952a7c857cda4017ce002a0d89beeb
anteqkois <anteqkois.dev@gmail.com>
2023-10-15 21:20:57 +0200

    chore(nest): fix nest build bugs

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConnectorsModule } from './modules/connectors/connectors.module'
-import { AuthModule, CoreModule, FlowsModule, UsersModule } from '@market-connector/core'
+import { AuthModule, ConnectorsMetadataModule, CoreModule, FlowsModule, UsersModule } from '@market-connector/core'
 
 @Module({
-  imports: [CoreModule, UsersModule, AuthModule, ConnectorsModule, FlowsModule],
+  imports: [CoreModule, UsersModule, AuthModule, ConnectorsModule, FlowsModule, ConnectorsMetadataModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/core/src/modules/connectors-metadata/connectors-metadata.module.ts
+++ libs/core/src/modules/connectors-metadata/connectors-metadata.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConnectorsMetadataService } from './connectors-metadata.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ConnectorsMetadataController } from './connectors-metadata.controller';
+import { ConnectorsMetadataService } from './connectors-metadata.service';
+import { ConnectorMetadataModelFactory } from './schemas/connector.schema';
 
 @Module({
+  imports: [MongooseModule.forFeatureAsync([ConnectorMetadataModelFactory])],
   controllers: [ConnectorsMetadataController],
   providers: [ConnectorsMetadataService]
 })
