2067d81fc44a2b9532d9c05056b19a66f36c550b
anteqkois <anteqkois.dev@gmail.com>
2023-12-26 21:34:54 +0100

    feat(connectors): add props connector to test auto form

---
+++ libs/connectors/props/README.md
@@ -0,0 +1,7 @@
+# connectors-props
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build connectors-props` to build the library.
---
+++ libs/connectors/props/src/lib/triggers/fetch-by-id.ts
@@ -0,0 +1,50 @@
+import { DedupeStrategy, Polling, Property, TriggerStrategy, createTrigger, pollingHelper } from '@market-connector/connectors-framework'
+import { coingeckoCommon } from '../common'
+
+const polling: Polling<any, any> = {
+  strategy: DedupeStrategy.LAST_ITEM,
+  items: async ({ auth, propsValue, lastItemId }) => {
+    const currentValues = (await coingeckoCommon.getCoins()) ?? []
+    const items = currentValues
+    // .filter((f) => Object.keys(f).length > 0)
+    // .map((item, index) => ({
+    //   id: index + 1,
+    //   data: item,
+    // }))
+    // .filter((f) => isNil(lastItemId) || f.data.row > (lastItemId as number))
+    return items.reverse()
+  },
+}
+
+export const fetchById = createTrigger({
+  description: 'Fetch by coingecko id',
+  displayName: 'Fetch by coingecko id',
+  name: 'fetch_by_id',
+  type: TriggerStrategy.POLLING,
+  onEnable: async (context) => {
+    await pollingHelper.onEnable(polling, {
+      auth: context.auth,
+      store: context.store,
+      propsValue: context.propsValue,
+    })
+  },
+  props: {
+    interval: Property.Number({
+      displayName: 'Interval',
+      name: 'minutes_interval',
+      required: true,
+      description: 'Every x minutes fetch data (min: 5, max: 60)',
+      validators: [],
+    }),
+  },
+  run: async (ctx) => {
+    return []
+  },
+  sampleData: {},
+  onDisable: async (ctx) => {
+    ctx.setSchedule({
+      cronExpression: '',
+      // timezone: ''
+    })
+  },
+})
