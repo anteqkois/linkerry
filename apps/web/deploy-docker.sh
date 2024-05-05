00d746355a96a1bc6b7339fc734ccc7114b1f117
anteqkois <anteqkois.dev@gmail.com>
2024-05-05 11:21:15 +0200

    chore(chore): save state

--- apps/web/deploy-docker.sh
+++ apps/web/deploy-docker.sh
@@ -5,8 +5,8 @@ set -e
 # Build images
 docker build -t linkerry/web -f ./apps/web/Dockerfile . --platform=linux/amd64 --progress=plain
 
-# Push docker image to container registry
-docker login -u "$CONTAINER_REGISTRY_USERNAME" -p "$CONTAINER_REGISTRY_PASSWORD" "$CONTAINER_REGISTRY_ADDRESS"
+# # Push docker image to container registry
+# docker login -u "$CONTAINER_REGISTRY_USERNAME" -p "$CONTAINER_REGISTRY_PASSWORD" "$CONTAINER_REGISTRY_ADDRESS"
 
-docker tag linkerry/web registry.digitalocean.com/linkerry/web:"$ENV"
-docker push registry.digitalocean.com/linkerry/web:"$ENV"
+# docker tag linkerry/web registry.digitalocean.com/linkerry/web:"$ENV"
+# docker push registry.digitalocean.com/linkerry/web:"$ENV"
