cd895015459ba21c235e45ef212d7c660caf9c8e
anteqkois <anteqkois.dev@gmail.com>
2024-05-06 23:49:08 +0200

    ci(docker): push updated docker builds

--- apps/api-gateway/deploy-docker.sh
+++ apps/api-gateway/deploy-docker.sh
@@ -2,11 +2,20 @@
 
 set -e
 
+# load env veriables
+eval "$(
+  cat .env | awk '!/^\s*#/' | awk '!/^\s*$/' | while IFS='' read -r line; do
+    key=$(echo "$line" | cut -d '=' -f 1)
+    value=$(echo "$line" | cut -d '=' -f 2-)
+    echo "export $key=\"$value\""
+  done
+)"
+
 # Build images
-docker build -t linkerry/api -f ./apps/api-gateway/Dockerfile . --platform=linux/amd64
+docker build -t linkerry/api-gateway -f ./apps/api-gateway/Dockerfile . --platform=linux/amd64
 
 # Push docker image to container registry
 docker login -u "$CONTAINER_REGISTRY_USERNAME" -p "$CONTAINER_REGISTRY_PASSWORD" "$CONTAINER_REGISTRY_ADDRESS"
 
-docker tag linkerry/api registry.digitalocean.com/linkerry/api:"$ENV"
-docker push registry.digitalocean.com/linkerry/api:"$ENV"
+docker tag linkerry/api-gateway registry.digitalocean.com/linkerry/api-gateway:latest
+docker push registry.digitalocean.com/linkerry/api-gateway:latest
