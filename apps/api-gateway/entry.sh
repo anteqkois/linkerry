e0f9523a21296841ae7e0aa56869e0b35adf6904
anteqkois <anteqkois.dev@gmail.com>
2024-05-26 23:04:27 +0200

    fix(env): comment delete env logic

--- apps/api-gateway/entry.sh
+++ apps/api-gateway/entry.sh
@@ -6,10 +6,10 @@ node ./dist/apps/api-gateway/main.js &
 # Capture the PID of the Node.js application
 NODE_PID=$!
 
-sleep 15
+# sleep 15
 
-# Clear file
-> ./.env
+# # Clear file
+# > ./.env
 
 # Wait for the Node.js application to finish (if needed)
 wait $NODE_PID
