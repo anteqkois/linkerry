bd145e5fe9b942e55349749a77a697342ac65b0e
anteqkois <anteqkois.dev@gmail.com>
2024-06-12 00:00:44 +0200

    fix(cors): try to handle cors error on the frontend

--- apps/web/libs/api-client.ts
+++ apps/web/libs/api-client.ts
@@ -1,3 +1,4 @@
+import { waitMs } from '@linkerry/shared'
 import axios from 'axios'
 // import { useRouter } from 'next/router';
 // import { store } from '../../features/common/store';
@@ -31,21 +32,12 @@ apiClient.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config
 
-    if (!error.response) {
-      console.log('API CLIENT ERROR')
-      console.log(error)
-      console.log(error.message)
-      console.log(error?.response)
-      console.dir(error, { depth: null })
-      // Network error or CORS error
-      // if (error.message === 'Network Error' || error.message.includes('CORS')) {
-      //   console.error('CORS error detected:', error.message)
-      //   window.location.href = '/cors-error'
-      //   return Promise.reject(error)
-      // }
-    }
-
-    if (error?.response?.status == 401 && error?.config && !error?.config?._isRetry) {
+    if (
+      // token expired
+      (error?.response?.status == 401 && error?.config && !error?.config?._isRetry) ||
+      // CORS occurs
+      ((error.message === 'Network Error' || error.message.includes('CORS')) && error?.config && !error?.config?._isRetry)
+    ) {
       originalRequest._isRetry = true
       try {
         const response = await axios.get<{ success: boolean }>(`${API_URL}/api/v1/auth/refresh`, {
@@ -54,7 +46,10 @@ apiClient.interceptors.response.use(
             // fingerprint: await fingerprint,
           },
         })
-        if (response.data.success) return apiClient.request(originalRequest)
+        if (response.data.success) {
+          await waitMs(500)
+          return apiClient.request(originalRequest)
+        }
         // Move user to login page, tell that session expired
         // clearAuthCookies()
         await redirectToLogin()
